3edfce2f77ef5b284974d5f466547a55661163f2
bilsinas <Nevnerzhitskaiadaryna@gmail.com>
2025-09-18 20:04:01 +0300

    .

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,28 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import model.Candidate;
+import java.util.function.Predicate;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    @Override
+    public boolean test(Candidate candidate) {
+        if (candidate.getAge() < 35 || !candidate.isAllowedToVote() || !"Ukrainian".equals(candidate.getNationality())) {
+            return false;
+        }
+
+        Pattern pattern = Pattern.compile("(\\d{4})-(\\d{4})");
+        Matcher matcher = pattern.matcher(candidate.getPeriodsInUkr());
+        if (matcher.find()) {
+            try {
+                int startYear = Integer.parseInt(matcher.group(1));
+                int endYear = Integer.parseInt(matcher.group(2));
+                return endYear - startYear >= 10;
+            } catch (NumberFormatException e) {
+                return false;
+            }
+        }
+        return false;
+    }
 }
